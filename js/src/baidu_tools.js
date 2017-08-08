846cf669553519a622313a59b554cf716da79f2e
cherry <991939332@qq.com>
2017-08-08 19:38:15 +0800

    Site updated: 2017-08-08 19:38:14

--- js/src/baidu_tools.js
+++ js/src/baidu_tools.js
@@ -1,1 +1,22 @@
-!function(){var t="http:"==document.location.protocol?"http://js.passport.qihucdn.com/11.0.1.js?940223423bbed5ceb474bec1058bc6fb":"https://jspassport.ssl.qhimg.com/11.0.1.js?940223423bbed5ceb474bec1058bc6fb";document.write('<script src="'+t+'" id="sozz"><\/script>')}(),function(){var t=document.createElement("script"),s=window.location.protocol.split(":")[0];t.src="https"===s?"https://zz.bdstatic.com/linksubmit/push.js":"http://push.zhanzhang.baidu.com/push.js";var c=document.getElementsByTagName("script")[0];c.parentNode.insertBefore(t,c)}();
+/**
+ * Created by cherry on 2017/7/20.
+ */
+//360站长平台
+(function(){
+    var src = (document.location.protocol == "http:") ? "http://js.passport.qihucdn.com/11.0.1.js?940223423bbed5ceb474bec1058bc6fb":"https://jspassport.ssl.qhimg.com/11.0.1.js?940223423bbed5ceb474bec1058bc6fb";
+    document.write('<script src="' + src + '" id="sozz"><\/script>');
+})();
+
+//百度站长
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https'){
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else{
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
